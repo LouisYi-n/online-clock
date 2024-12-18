4a1e23c123d8d45d179956c685ac0263f42a5d2a
louis.yi <louis.yi@qima.com>
2024-12-18 18:05:37 +0800

    update words

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Stopwatch: React.FC = () => {
-    return <div>树叶落下的速度是秒速五厘米，你的只有1秒长</div>;
+    return <div>树叶落下的速度是秒速五厘米，一棵树秃顶只需要一个秋天。<br/><br/>那么，你呢？</div>;
 };
 
 export default Stopwatch;
